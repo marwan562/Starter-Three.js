ede47812b07fed163c161a402063494a36496b66
marwan562 <mixing.gamer546@gmail.com>
2024-11-06 21:04:14 +0200

    axis helper

--- main.js
+++ main.js
@@ -5,9 +5,16 @@ const scene = new THREE.Scene();
 
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
-
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cubeMesh);
+cubeMesh.position.y = 1
+cubeMesh.position.x = 1
+cubeMesh.position.z = 1
+
+// axes line helper
+const axesHelper = new THREE.AxesHelper(10);
+
+// add scenes here
+scene.add(cubeMesh, axesHelper);
 
 //initialize the camera here
 const camera = new THREE.PerspectiveCamera(
@@ -22,23 +29,18 @@ scene.add(camera);
 
 //initialize the renderer here
 const canvas = document.querySelector("canvas.threejs");
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-
+const MAX_PIXEL_RATIO = Math.min(window.devicePixelRatio, 2);
+renderer.setPixelRatio(MAX_PIXEL_RATIO);
 
 //initialize the orbit controls
 const orbitControl = new OrbitControls(camera, renderer.domElement);
-orbitControl.enableDamping = true 
-orbitControl.autoRotate = true
 
-
-function animatLoopFrame  () {
-  window.requestAnimationFrame(animatLoopFrame)
-  orbitControl.update()
+function animatLoopFrame() {
+  window.requestAnimationFrame(animatLoopFrame);
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.render(scene, camera);
 }
 
-// animatLoopFrame()
-
-
-
+animatLoopFrame();
