1882ba7c05f39f908d8e2fec2c2b0e5fdfd4f213
marwan562 <mixing.gamer546@gmail.com>
2024-11-08 23:05:38 +0200

    simple animation

--- main.js
+++ main.js
@@ -3,28 +3,42 @@ import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
 
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cubeMesh.position.y = 1
-cubeMesh.position.x = 1
-cubeMesh.position.z = 1
+//axes helper line
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper )
 
-// axes line helper
-const axesHelper = new THREE.AxesHelper(10);
+//cube mesh
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cubeGeometry = new THREE.BoxGeometry(4, 4, 4, 512, 512, 512);
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-// add scenes here
-scene.add(cubeMesh, axesHelper);
+//! Sphere
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const sphereGeometry = new THREE.SphereGeometry(2, 512, 512);
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.x = 10;
+
+//! turos
+const toursMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const toursGeometry = new THREE.TorusGeometry(2, 0.4, 512, 512);
+const tours = new THREE.Mesh(toursGeometry, toursMaterial);
+tours.position.x = -10;
+
+// group all mesh
+const group = new THREE.Group() 
+group.add(cube , tours , sphere)
+
+scene.add(group)
 
 //initialize the camera here
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  35
+  1000
 );
 
-camera.position.z = 5;
+camera.position.set(0, 0, 10);
 scene.add(camera);
 
 //initialize the renderer here
@@ -37,9 +51,30 @@ renderer.setPixelRatio(MAX_PIXEL_RATIO);
 //initialize the orbit controls
 const orbitControl = new OrbitControls(camera, renderer.domElement);
 
+//clock time for animations frams optmize with difference devices
+const clock = new THREE.Clock()
+
+
 function animatLoopFrame() {
   window.requestAnimationFrame(animatLoopFrame);
   renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // elapsed time 
+  const elapsedTime = clock.getElapsedTime()
+
+  // simple animation 
+  cube.rotation.y = elapsedTime * .20
+  cube.rotation.x = elapsedTime * .20
+  cube.rotation.z = elapsedTime * .20
+
+  sphere.rotation.y = elapsedTime * .20
+  sphere.rotation.x = elapsedTime * .20
+  sphere.rotation.z = elapsedTime * .20
+
+  tours.rotation.y = elapsedTime * .20
+  tours.rotation.x = elapsedTime * .20
+  tours.rotation.z = elapsedTime * .20
+
   renderer.render(scene, camera);
 }
 
