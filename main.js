c5c27b51a680cce5924ed63282dc9595200946a7
marwan562 <mixing.gamer546@gmail.com>
2024-11-09 00:55:39 +0200

    part 1

--- main.js
+++ main.js
@@ -3,32 +3,37 @@ import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
 
+// texture loader
+const textureLoader = new THREE.TextureLoader();
+const colorMap = textureLoader.load("/static/textures/1/color.jpg");
+
+
 //axes helper line
-const axesHelper = new THREE.AxesHelper(5)
-scene.add(axesHelper )
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
 
 //cube mesh
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cubeMaterial = new THREE.MeshBasicMaterial({ map: colorMap});
 const cubeGeometry = new THREE.BoxGeometry(4, 4, 4, 512, 512, 512);
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
 //! Sphere
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const sphereMaterial = new THREE.MeshBasicMaterial({ map: colorMap });
 const sphereGeometry = new THREE.SphereGeometry(2, 512, 512);
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 sphere.position.x = 10;
 
 //! turos
-const toursMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const toursMaterial = new THREE.MeshBasicMaterial({ map: colorMap });
 const toursGeometry = new THREE.TorusGeometry(2, 0.4, 512, 512);
 const tours = new THREE.Mesh(toursGeometry, toursMaterial);
 tours.position.x = -10;
 
 // group all mesh
-const group = new THREE.Group() 
-group.add(cube , tours , sphere)
+const group = new THREE.Group();
+group.add(cube, tours, sphere);
 
-scene.add(group)
+scene.add(group);
 
 //initialize the camera here
 const camera = new THREE.PerspectiveCamera(
@@ -43,37 +48,34 @@ scene.add(camera);
 
 //initialize the renderer here
 const canvas = document.querySelector("canvas.threejs");
-const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+const renderer = new THREE.WebGLRenderer({ canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
-const MAX_PIXEL_RATIO = Math.min(window.devicePixelRatio, 2);
-renderer.setPixelRatio(MAX_PIXEL_RATIO);
 
 //initialize the orbit controls
 const orbitControl = new OrbitControls(camera, renderer.domElement);
 
 //clock time for animations frams optmize with difference devices
-const clock = new THREE.Clock()
-
+const clock = new THREE.Clock();
 
 function animatLoopFrame() {
   window.requestAnimationFrame(animatLoopFrame);
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  // elapsed time 
-  const elapsedTime = clock.getElapsedTime()
+  // elapsed time
+  const elapsedTime = clock.getElapsedTime();
 
-  // simple animation 
-  cube.rotation.y = elapsedTime * .20
-  cube.rotation.x = elapsedTime * .20
-  cube.rotation.z = elapsedTime * .20
+  // simple animation
+  cube.rotation.y = elapsedTime * 0.2;
+  cube.rotation.x = elapsedTime * 0.2;
+  cube.rotation.z = elapsedTime * 0.2;
 
-  sphere.rotation.y = elapsedTime * .20
-  sphere.rotation.x = elapsedTime * .20
-  sphere.rotation.z = elapsedTime * .20
+  sphere.rotation.y = elapsedTime * 0.2;
+  sphere.rotation.x = elapsedTime * 0.2;
+  sphere.rotation.z = elapsedTime * 0.2;
 
-  tours.rotation.y = elapsedTime * .20
-  tours.rotation.x = elapsedTime * .20
-  tours.rotation.z = elapsedTime * .20
+  tours.rotation.y = elapsedTime * 0.2;
+  tours.rotation.x = elapsedTime * 0.2;
+  tours.rotation.z = elapsedTime * 0.2;
 
   renderer.render(scene, camera);
 }
