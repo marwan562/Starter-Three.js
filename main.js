2104ee7b77a004cde968cd32b7de75e7b3816ced
marwan562 <mixing.gamer546@gmail.com>
2024-11-08 18:57:05 +0200

    mouse move animation

--- main.js
+++ main.js
@@ -2,44 +2,43 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-
-const environment = cubeTextureLoader.load([
-  "./Standard-Cube-Map/nx.png",
-  "./Standard-Cube-Map/ny.png",
-  "./Standard-Cube-Map/nz.png",
-  "./Standard-Cube-Map/px.png",
-  "./Standard-Cube-Map/py.png",
-  "./Standard-Cube-Map/pz.png",
-]);
-
-scene.background = environment;
 
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({
   color: "red",
 });
+const cubeMaterial1 = new THREE.MeshBasicMaterial({
+  color: "yellow",
+});
 
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cubeMesh.position.y = 2;
-cubeMesh.rotation.x = 1.57;
+const cubeMesh1 = new THREE.Mesh(cubeGeometry, cubeMaterial1);
+cubeMesh1.position.x = -2;
 
-scene.add(cubeMesh);
+scene.add(cubeMesh, cubeMesh1);
 
 const aspectCamera = window.innerWidth / window.innerHeight;
 
-//initialize the camera here
+// initialize the camera here
 const camera = new THREE.PerspectiveCamera(75, aspectCamera, 0.1, 100);
 
-camera.position.x = 3;
-camera.position.z = 3;
-camera.position.y = 3;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectCamera,
+//   1 * aspectCamera,
+//   1,
+//   -1,
+//   0.1,
+  100
+// );
+
+camera.position.x = -3;
 
 scene.add(camera);
 
 // axises helper
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
+
 
 //initialize the renderer here
 const canvas = document.querySelector("canvas.threejs");
@@ -52,6 +51,16 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
 });
 
+const cursorMouse = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (ev) => {
+  cursorMouse.x = ev.clientX / window.innerWidth - 0.5;
+  cursorMouse.y = ev.clientY / window.innerHeight - 0.5;
+});
+
 //initialize the orbit controls
 const orbitControl = new OrbitControls(camera, renderer.domElement);
 orbitControl.enableDamping = true;
@@ -59,6 +68,13 @@ orbitControl.autoRotate = true;
 
 function animatLoopFrame() {
   window.requestAnimationFrame(animatLoopFrame);
+  // camera.position.x =  cursorMouse.x * 10
+  // camera.position.y = -  cursorMouse.y *10
+  camera.position.x = Math.sin(Math.PI * 2 * cursorMouse.x) * 10;
+  camera.position.z = Math.cos(Math.PI * 2 * cursorMouse.x) * 10;
+  camera.position.y = cursorMouse.y * 20;
+
+  camera.lookAt(0, 0, 0);
   renderer.render(scene, camera);
 }
 
