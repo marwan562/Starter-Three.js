5337b62dbe2bfbf0133a10faaead08c47de0c0df
marwan562 <mixing.gamer546@gmail.com>
2024-11-19 12:37:00 +0200

    All GLFT with animation-fox

--- main.js
+++ main.js
@@ -1,17 +1,83 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 // Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 
-// GLTF Loader
-const gltfLoader = new GLTFLoader();
-let model;
-gltfLoader.load("/robotic_eye/scene.gltf", (gltf) => {
-  model = gltf.scene;
-  scene.add(model);
+/**
+ * GLFT Loader
+ */
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderConfig({ type: "js" });
+dracoLoader.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
+const glftLoader = new GLTFLoader();
+glftLoader.setDRACOLoader(dracoLoader);
+
+/**
+ * Ducks
+ */
+glftLoader.load("/models/Duck/glTF/Duck.gltf", (glft) => {
+  scene.add(glft.scene);
+});
+
+glftLoader.load("/models/Duck/glTF-Binary/Duck.glb", (glft) => {
+  glft.scene.position.x = 2;
+  scene.add(glft.scene);
+});
+
+glftLoader.load("/models/Duck/glTF-Embedded/Duck.gltf", (glft) => {
+  glft.scene.position.x = 4;
+  scene.add(glft.scene);
+});
+
+glftLoader.load("/models/Duck/glTF-Draco/Duck.gltf", (glft) => {
+  glft.scene.position.x = 6;
+  scene.add(glft.scene);
+});
+
+/**
+ * Fox
+ */
+let mixer1;
+let mixer2;
+let mixer3;
+
+glftLoader.load("/models/Fox/glTF/Fox.gltf", (glft) => {
+  glft.scene.position.x = -2;
+  glft.scene.scale.set(0.1, 0.1, 0.1);
+  scene.add(glft.scene);
+  mixer1 = new THREE.AnimationMixer(glft.scene);
+  const action = mixer1.clipAction(glft.animations[0]);
+  action.play();
+});
+
+glftLoader.load("/models/Fox/glTF-Binary/Fox.glb", (glft) => {
+  glft.scene.position.x = -6;
+  glft.scene.scale.set(0.1, 0.1, 0.1);
+  scene.add(glft.scene);
+  mixer2 = new THREE.AnimationMixer(glft.scene);
+  const action = mixer2.clipAction(glft.animations[1]);
+  action.play();
+});
+
+glftLoader.load("/models/Fox/glTF-Embedded/Fox.gltf", (glft) => {
+  glft.scene.position.x = -10;
+  glft.scene.scale.set(0.1, 0.1, 0.1);
+  scene.add(glft.scene);
+  mixer3 = new THREE.AnimationMixer(glft.scene);
+  const action = mixer3.clipAction(glft.animations[2]);
+  action.play();
+});
+
+/**
+ * Flight helmet
+ */
+glftLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (glft) => {
+  glft.scene.position.y = 3;
+  scene.add(glft.scene);
 });
 
 // Camera
@@ -21,22 +87,8 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   2000
 );
-camera.position.z = 0.2;
+camera.position.set(6, 6, 6);
 scene.add(camera);
-
-/**
- * Mouse Movement
- */
-const mouse = { x: 0, y: 0 };
-window.addEventListener("mousemove", (e) => {
-  mouse.x = e.clientX / window.innerWidth - 0.5;
-  mouse.y = e.clientY / window.innerHeight - 0.5;
-
-  if (model) {
-    model.rotation.y = (mouse.x * Math.PI) / 2;
-    model.rotation.x = (mouse.y * Math.PI) / 2;
-  }
-});
 
 /**
  * Lights
@@ -69,8 +121,23 @@ window.addEventListener("resize", () => {
 
 // Animation Loop
 const clock = new THREE.Clock();
+let previousTime = 0;
+
 function animateLoopFrame() {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+
+  if (mixer1) {
+    mixer1.update(deltaTime);
+  }
+  if (mixer2) {
+    mixer2.update(deltaTime);
+  }
+  if (mixer3) {
+    mixer3.update(deltaTime);
+  }
+
   // Render scene
   orbitControls.update();
   renderer.render(scene, camera);
